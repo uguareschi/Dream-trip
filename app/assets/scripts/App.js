0a1de27e05f70d4ea086335e82aebc89fc9f1610
Ulises <uguareschi@gmail.com>
2020-07-28 16:41:17 +0200

    Completed StickyHeader

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,11 +1,13 @@
 import '../styles/styles.css';
 import MobileMenu from './modules/MobileMenu';
 import RevealOnScroll from './modules/RevealOnScroll';
+import StickyHeader from './modules/StickyHeader';
 
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
 
 let mobileMenu = new MobileMenu();
+let stickyHeader = new StickyHeader();
 
 if(module.hot) {
     module.hot.accept();
