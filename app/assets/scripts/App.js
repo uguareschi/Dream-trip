0006d1098821570cdbd9266a4c0a4d2a1ba2ffc3
Ulises <uguareschi@gmail.com>
2020-07-30 18:14:36 +0200

    Completed Modal Area

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -5,9 +5,23 @@ import StickyHeader from './modules/StickyHeader';
 
 new RevealOnScroll(document.querySelectorAll('.feature-item'), 75);
 new RevealOnScroll(document.querySelectorAll('.testimonial'), 60);
+new MobileMenu();
+new StickyHeader();
+let modal;
 
-let mobileMenu = new MobileMenu();
-let stickyHeader = new StickyHeader();
+document.querySelectorAll('.open-modal').forEach(el => {
+    el.addEventListener('click', e => {
+        e.preventDefault();
+        if(typeof modal == 'undefined') {
+            import('./modules/Modal').then((x) => {
+                modal =  new x.default()
+                setTimeout(() => modal.openTheModal(), 20)
+            }).catch(() => console.log('There was a problem.'))
+        } else {
+            modal.openTheModal();
+        }
+    })
+})
 
 if(module.hot) {
     module.hot.accept();
